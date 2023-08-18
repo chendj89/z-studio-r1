d1a73e339f5cd4fe9e3160fd289465ff534e6168
九转灵山 <chendj89@hotmail.com>
2023-08-18 20:38:22 +0800

    zoom模式

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,13 @@
   <router-view></router-view>
 </template>
 
-<script setup name="App"></script>
+<script setup name="App">
+import { addResizeListener } from './utils/resize';
+const resize=()=>{
+  const zoom=window.devicePixelRatio/1;
+  document.documentElement.style.setProperty('zoom', 1/zoom)
+}
+addResizeListener(resize)
+</script>
 
 <style lang="scss" scoped></style>
