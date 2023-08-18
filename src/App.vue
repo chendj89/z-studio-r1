124600439f5fa6f985987bad46f4335ccd9193ce
九转灵山 <chendj89@hotmail.com>
2023-08-18 21:23:02 +0800

    zoom模式

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,21 @@
   <router-view></router-view>
 </template>
 
-<script setup name="App"></script>
+<script setup name="App">
+import { onMounted } from 'vue'
+import { addResizeListener } from './utils/resize'
+const resize = () => {
+  const zoom = window.devicePixelRatio / 1
+  document.documentElement.style.setProperty('zoom', 1 / zoom)
+  localStorage.setItem('zoom', zoom)
+}
+onMounted(() => {
+  const localZoom = localStorage.getItem('zoom')
+  if (localZoom) {
+    document.documentElement.style.setProperty('zoom', 1 / localZoom)
+  }
+})
+addResizeListener(resize)
+</script>
 
 <style lang="scss" scoped></style>
